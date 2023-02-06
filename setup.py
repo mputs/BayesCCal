3297817bd67e8b9daf36f94e5ecfe5584b0bddcf
Marco Puts <marco.puts@gmail.com>
2023-02-06 11:19:34 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BayesCCal',
+      version='0.1',
+      description='Bayesian calibration of classifiers',
+      author='Marco Puts',
+      author_email='mputs@acm.org',
+      packages=['BayesCCal'],
+     )
